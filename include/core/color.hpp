26cb01c07ba9120fcdebf40dcc9c1bb2e6b03001
Ihor Zvieriev <naomipappe.zvieriev@gmail.com>
2025-04-12 22:33:32 +0200

    Gamma correction + fix for narrowing conversion

--- include/core/color.hpp
+++ include/core/color.hpp
@@ -16,9 +16,9 @@ struct rgb_color : public vec3f
     // This is stupid, but it works
     rgb_color(const vec3f& base) : vec3f(base) {}
 
-    u8 r() const { return std::clamp(m_data[0], 0.0f, 0.999f) * 256; }
-    u8 g() const { return std::clamp(m_data[1], 0.0f, 0.999f) * 256; }
-    u8 b() const { return std::clamp(m_data[2], 0.0f, 0.999f) * 256; }
+    f32 r() const { return m_data[0]; }
+    f32 g() const { return m_data[1]; }
+    f32 b() const { return m_data[2]; }
 };
 
 inline std::ostream& operator<<(std::ostream& out, const rgb_color& color)
