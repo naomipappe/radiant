f8944a54b11d6d4ee319ea995aaadb557dca8e00
naomipappe <naomiappe.zvieriev@gmail.com>
2026-06-23 00:18:10 +0200

    Hammered in the ImGui edit PoC to edit emissive component in materials

--- include/core/scene.hpp
+++ include/core/scene.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "imgui.h"
+
 #include "core/material.hpp"
 #include "core/primitive.hpp"
-#include "core/triangle_mesh.hpp"
+#include "shapes/triangle.hpp"
 
 #include <memory>
 #include <vector>
@@ -12,8 +14,18 @@ namespace radiant
 
 struct Scene
 {
-    std::vector<std::shared_ptr<StaticTriangleMesh>> m_meshes;
-    std::vector<std::shared_ptr<Material>>           m_materials;
+    void imgui_traverse()
+    {
+        double min = 0.0f;
+        double max = 1.0f;
+        for (const auto& obj : m_materials)
+        {
+            m_dirty = m_dirty ||  ImGui::SliderScalarN("RGB", ImGuiDataType_Double, &obj->m_emissive.m_data[0], 3,&min, &max, "%.3f");
+        }
+    }
+    bool                                    m_dirty{ false };
+    std::vector<std::shared_ptr<Mesh>>      m_meshes;
+    std::vector<std::shared_ptr<Material>>  m_materials;
     std::vector<std::shared_ptr<Primitive>> m_primitives;
 };
 
